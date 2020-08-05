a42e55f8f37d046886c72b4de9734a2a0677d2ab
cyberghosts <yee.darren@gmail.com>
2020-08-04 20:26:46 -0600

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,11 +44,6 @@ module.exports = {
             sizes: `192x192`,
             type: `image/png`,
           },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
         ],
       },
     },
